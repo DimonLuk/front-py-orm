949c7d3901c69ca7b4a77d63f70e6611fbe192c7
dimonluk <lds4ever2000@gmail.com>
2018-08-07 11:54:05 +0300

    Made getters and setters for updating and function add to add new records to db. Need to modify DBConnection

--- TableMeta.py
+++ TableMeta.py
@@ -1,47 +1,55 @@
 import copy
+import datetime
+import hashlib
 import sqlite3
 
 from Types import Types
 
 
+class DBConnection:
+
+    def __init__(self, db_name="data.db", username="", password=""):
+        self.db_name = db_name
+        self.username = username
+        self.password = password
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit()
+        self.connection.close()
+
+
 def get_value(self, name):
-    connection = sqlite3.connect("data.db")
-    cursor = connection.cursor()
-    result = cursor.execute("""SELECT * FROM {0} where {1}='{2}'"""
-                    .format(self.__table_name__, "__uid__", self.__uid__)).fetchall()[0]
-    names = cursor.execute("PRAGMA table_info({0})".format(self.__table_name__)).fetchall()
-    for i in range(len(names)):
-        if names[i][1] == name:
-            return result[i]
+    with DBConnection() as cursor:
+        result = cursor.execute("""SELECT * FROM {0} where {1}='{2}'"""
+                        .format(self.__table_name__, "__uid__", self.__uid__)).fetchall()[0]
+        names = cursor.execute("PRAGMA table_info({0})".format(self.__table_name__)).fetchall()
+        for i in range(len(names)):
+            if names[i][1] == name:
+                return result[i]
 
 
 def set_value(self, name, value):
-    if name.startswith("__"):
+    if name.startswith("__") and name != "__uid__":
         self.__dict__[name] = value
-    else:
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-        try:
-            int(value)
-            cursor.execute("""UPDATE {0} SET {1}={2} WHERE __uid__='{3}'"""
-                        .format(self.__table_name__, name, value, self.__uid__))
-        except ValueError:
-            try:
-                float(value)
-                cursor.execute("""UPDATE {0} SET {1}={2} WHERE __uid__='{3}'"""
-                            .format(self.__table_name__, name, value, self.__uid__))
-            except ValueError:
+    elif name != "__uid__":
+        with DBConnection() as cursor:
+            if type(value) != float and type(value) != int:
                 cursor.execute("""UPDATE {0} SET {1}='{2}' WHERE __uid__='{3}'"""
                                .format(self.__table_name__, name, str(value), self.__uid__))
-        connection.commit()
-        connection.close()
+            else:
+                cursor.execute("""UPDATE {0} SET {1}={2} WHERE __uid__='{3}'"""
+                               .format(self.__table_name__, name, str(value), self.__uid__))
 
 
 def get(table_name):
     def inner(self, **keys):
         execute_str = """SELECT * FROM %s WHERE """
-        components = []
-        components.append(table_name)
+        components = [table_name]
         for i in keys:
             try:
                 float(keys[i])
@@ -53,25 +61,19 @@ def get(table_name):
                 components.append(i)
                 components.append(keys[i])
         execute_str = execute_str[:-4]
-        # print(execute_str)
-        # print(components)
         execute_str = execute_str % tuple(components)
-        # print(execute_str)
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-        cursor.execute(execute_str.format(*components))
-        res = cursor.fetchall()
-        names = cursor.execute("PRAGMA table_info({0})".format(table_name)).fetchall()
-        connection.close()
-        result = list()
-        obj_name = table_name.split("__")[0]
-        for k in res:
-            obj = {"__table_name__": table_name, "__getattr__": get_value, "__uid__": k[0], "__setattr__": set_value}
-            result.append(type(obj_name, tuple(), obj)())
-        if len(result) == 1:
-            return result[0]
-        else:
-            return result
+        with DBConnection() as cursor:
+            cursor.execute(execute_str.format(*components))
+            res = cursor.fetchall()
+            result = list()
+            obj_name = table_name.split("__")[0]
+            for k in res:
+                obj = {"__table_name__": table_name, "__getattr__": get_value, "__uid__": k[0], "__setattr__": set_value}
+                result.append(type(obj_name, tuple(), obj)())
+            if len(result) == 1:
+                return result[0]
+            else:
+                return result
     return inner
 
 
@@ -106,17 +108,14 @@ class ModelMeta(type):
         dct_["get_by"] = get(name)
         try:
             create_str = create_str[:-1] + ")"
-            connection = sqlite3.connect("data.db")
-            cursor = connection.cursor()
-            cursor.execute(create_str)
-            if add_column_flag:
-                for i in items_to_add:
-                    add_column_str = """ALTER TABLE {0} ADD COLUMN {1} '{2}'""".format(name, i, dct_[i])
-                    cursor.execute(add_column_str)
-            with open(name+"__info.data", "w") as file:
-                file.write(",".join(fields_arr))
-            connection.commit()
-            connection.close()
+            with DBConnection as cursor:
+                cursor.execute(create_str)
+                if add_column_flag:
+                    for i in items_to_add:
+                        add_column_str = """ALTER TABLE {0} ADD COLUMN {1} '{2}'""".format(name, i, dct_[i])
+                        cursor.execute(add_column_str)
+                with open(name+"__info.data", "w") as file:
+                    file.write(",".join(fields_arr))
         except sqlite3.OperationalError:
             pass
         finally:
@@ -124,7 +123,21 @@ class ModelMeta(type):
 
 
 class Model(metaclass=ModelMeta):
-    pass
+    def add(self, obj):
+        with DBConnection() as cursor:
+            to_execute = "INSERT INTO {0} (".format(type(self).__name__)
+            hash_ = str(hashlib.sha256((str(datetime.datetime.now())+str(obj)).encode("utf-8")).hexdigest())
+            obj["__uid__"] = hash_
+            for key in obj:
+                to_execute += key + ","
+            to_execute = to_execute[:-1] + ") VALUES ("
+            for key in obj:
+                if type(obj[key]) != float and type(obj[key]) != int:
+                    to_execute += "'" + str(obj[key]) + "'" + ","
+                else:
+                    to_execute += str(obj[key])+","
+            to_execute = to_execute[:-1] + ")"
+            cursor.execute(to_execute)
 
 
 class Person(Model):
@@ -134,8 +147,6 @@ class Person(Model):
 
 
 t = Person()
-dima = t.get_by(name="Dima")
-print(dima.age)
-# dima.age = 10
-# print(dima.age)
+obj = t.get_by(fullname="Lets")
+print(obj.__uid__)
 
